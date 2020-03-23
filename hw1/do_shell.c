5cac5a439da3a7fb7f77bb0932176207860af434
25077667 <zxc25077667@gmail.com>
2020-03-23 13:32:19 +0800

    Add info to notice IN_MY_SHELL

--- hw1/do_shell.c
+++ hw1/do_shell.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char **argv, char **envp){
-    return execve("/bin/bash", argv, envp);
+    printf("YOU ARE IN MY SHELL!\n");
+    return execve("/bin/sh", argv, envp);
 }
