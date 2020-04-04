933d60d74fa6519b919e11528b907ef4b4778063
25077667 <zxc25077667@gmail.com>
2020-04-04 21:03:48 +0800

    Processing command in single command handler
    Add command parser
    Add error raiseer
    Use linked-list to get I/O around this object

--- hw1/part2/proc.cpp
+++ hw1/part2/proc.cpp
@@ -2,10 +2,50 @@
 #include <cstring>
 #include <vector>
 using namespace std;
+#define BUFFER_SIZE 10000
+#define CHECK_FILE_EXIST(x) (x);
 
 Proc::Proc()
 {
-    this->in_fd = this->out_fd = this->err_fd = NULL;
+    this->in_fd = stdin;
+    this->out_fd = stdout;
+    this->err_fd = stderr;
+
+    this->prev = nullptr;
+    this->next = nullptr;
+}
+
+Proc::Proc(Proc *left)
+{
+    this->in_fd = stdin;
+    this->out_fd = stdout;
+    this->err_fd = stderr;
+
+    this->prev = left;
+    this->next = nullptr;
+    if (left)
+        left->next = this;
+}
+
+Proc::Proc(Proc *left, Proc *right)
+{
+    this->in_fd = stdin;
+    this->out_fd = stdout;
+    this->err_fd = stderr;
+
+    this->prev = left;
+    this->next = right;
+    if (left)
+        left->next = this;
+    if (right)
+        right->prev = this;
+}
+
+Proc::~Proc()
+{
+    fclose(this->in_fd);
+    fclose(this->out_fd);
+    fclose(this->err_fd);
 }
 
 void Proc::setSTDIN(FILE *set)
@@ -40,16 +80,25 @@ void Proc::setAllIO(FILE *_in, FILE *_out, FILE *_err)
  */
 FILE *Proc::doExecute()
 {
-    char tmp[1000] = {0};
+    char tmp[BUFFER_SIZE] = {0};
     /* Append perverious command result to the next command*/
     /* Just like pipe line*/
-    while (fscanf(this->in_fd, tmp) != EOF)
+    while (CHECK_FILE_EXIST(this->in_fd) && !feof(this->in_fd) &&
+           fgets(tmp, BUFFER_SIZE, this->in_fd)) {
         this->command.append(tmp);
-
-    return popen(this->command.c_str(), "r");  // The out_fd (read only)
+        memset(tmp, 0, BUFFER_SIZE);
+    }
+    auto will_be_out_fd = popen(this->command.c_str(), "r");
+    this->out_fd = will_be_out_fd;
+    if (this->next)
+        this->next->in_fd = will_be_out_fd;
+    return will_be_out_fd;  // The out_fd (read only)
 }
 
 /**
+ * From input stream to self input buffer, output buffer to the output stream.
+ * This function will not close your file.
+ *
  * This function actually do the I/O stream,
  * send and receive streams.
  */
@@ -58,9 +107,10 @@ void Proc::applyIO(FILE *source, FILE *distiantion)
     int errorCode = 0;
     if (source) {
         this->in_fd = source;
-        char buf[10000];
-        while (fscanf(source, buf) != EOF) {
-            this->in_buf.append(buf);
+        char tmp[BUFFER_SIZE] = {0};
+        while (!feof(source) && fgets(tmp, BUFFER_SIZE, source)) {
+            this->in_buf.append(tmp);
+            memset(tmp, 0, BUFFER_SIZE);
         }
         errorCode = 0;
     }
@@ -80,15 +130,56 @@ void Proc::applyIO(FILE *source, FILE *distiantion)
  * right 2 btyes are error class, using `rb` to description
  *
  * From the right to the left speaking
+ * rb:
+ * "0000 0000 0000 0001" File error
+ * 0000 0000 0000 0000 0000 0000 0000 0001: file is not exist or permission \
+ *                                           denied
+ * 0000 0000 0000 0001 0000 0000 0000 0001: command not found
  *
  */
 void Proc::raiseError(int errorCode)
 {
-    if (errorCode) {
+    switch (errorCode) {
+    case 1:
+        perror("File is not exist or permission denied\n");
+        break;
+
+    default:
+        break;
     }
 }
 
-inline void redirection(FILE *in_fd, FILE *out_fd)
+void Proc::commandParser()
 {
-    fprintf(out_fd, in_fd);
+    int errorCode = 0;
+    /**
+     * This identifier will get the last word to redirection
+     *
+     * TODO: support stderr redirection, multi-redirection
+     * string identifier;
+     */
+
+    for (auto iter = this->command.end() - 1;
+         !this->command.empty() && iter != this->command.begin(); iter--) {
+        char c = *iter;
+        auto _in_fd = (this->prev) ? this->prev->out_fd : stdin;
+        auto _out_fd = (this->next) ? this->next->in_fd : stdout;
+        if (c == ';') {
+            setAllIO(_in_fd, stdout, stderr);
+            break;
+        } else if (c == '>') {
+            setAllIO(_in_fd, _out_fd, stderr);
+            break;
+        } else if (c == '<') {
+            if (this->next == nullptr)
+                errorCode = 1;
+            this->next->out_fd = fopen(this->next->command.c_str(), "r");
+            setAllIO(this->next->out_fd, stdout, stderr);
+            break;
+        } else if (c == '|') {
+            setAllIO(_in_fd, _out_fd, stderr;
+            break;
+        }
+        raiseError(errorCode);
+    }
 }
--- hw1/part2/proc.hpp
+++ hw1/part2/proc.hpp
@@ -8,20 +8,24 @@ class Proc
 {
 public:
     Proc();
+    Proc(Proc *);
+    Proc(Proc *, Proc *);
+    ~Proc();
     void setSTDIN(FILE *);
     void setSTDOUT(FILE *);
     void setSTDERR(FILE *);
     void setAllIO(FILE *, FILE *, FILE *);
     void applyIO(FILE *, FILE *);
     FILE *doExecute();  // Pass file descriptors to destination
-    void redirection(FILE *, FILE *);
-
+    void commandParser();
+    Proc *prev, *next;
 
 private:
     string command, in_buf, out_buf, err_buf;
     int status;
     FILE *in_fd, *out_fd, *err_fd;
     void raiseError(int);
+    
 };
 
 #endif
