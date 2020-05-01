402d5cb8b33f33c6591d731af45dde5659624833
25077667 <zxc25077667@gmail.com>
2020-05-01 18:27:23 +0800

    Add monitor to replace the sleepAndGetInfo

--- hw3/part2/philosopher.cpp
+++ hw3/part2/philosopher.cpp
@@ -13,45 +13,28 @@
 
 using namespace std;
 
-static int random_func()
-{
+static int random_func() {
     random_device rd;
     auto gen = mt19937_64(rd());
     uniform_int_distribution<int> dis(1, 101);
     return bind(dis, gen)();
 }
 
-static void printStatus(const Philosopher philo, int t)
-{
-    cout << "id: " << (int) philo.id << " due: " << t
-         << " status: " << philo.state << endl;
-}
-
-static void sleepAndGetInfo(const Philosopher p)
-{
-    auto t = random_func();
-    printStatus(p, t);
-    usleep(t);
-}
-
 Philosopher::Philosopher(char _id) : id(_id), state("Confused") {}
 
-void Philosopher::think()
-{
+void Philosopher::think() {
     this->state = string("Confused");
-    sleepAndGetInfo(*this);
+    usleep(random_func());
     this->state = string("Hungry");
 }
 
-void Philosopher::eat()
-{
+void Philosopher::eat() {
     this->state = string("Hungry");
-    sleepAndGetInfo(*this);
+    usleep(random_func());
     this->state = string("Confused");
 }
 
-void Philosopher::useChop(vector<unique_ptr<Semaphore>> &chopPool)
-{
+void Philosopher::useChop(vector<unique_ptr<Semaphore>> &chopPool) {
     /* Show the waiting time to get chopsticks */
     auto start = chrono::steady_clock::now();
 
@@ -59,7 +42,7 @@ void Philosopher::useChop(vector<unique_ptr<Semaphore>> &chopPool)
     chopPool.at((this->id + 1) % PHILO_NUM).get()->wait();
 
     auto end = std::chrono::steady_clock::now();
-    cout << (int) this->id << " takes "
+    cout << (int)this->id << " takes "
          << chrono::duration<double>(end - start).count()
          << " second(s) for waiting chopsticks." << endl;
 
@@ -70,8 +53,7 @@ void Philosopher::useChop(vector<unique_ptr<Semaphore>> &chopPool)
     chopPool.at((this->id + 1) % PHILO_NUM).get()->notify();
 }
 
-void lifeTime(Philosopher &p, vector<unique_ptr<Semaphore>> &chopPool)
-{
+void lifeTime(Philosopher &p, vector<unique_ptr<Semaphore>> &chopPool) {
     // In 2/3 probability to go to eat
     bool isHungry = random_func() % 3;
     if (isHungry)
