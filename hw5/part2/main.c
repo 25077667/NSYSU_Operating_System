ae05ae37ce5b8729aa1e47483bf807547a8b0a71
25077667 <zxc25077667@gmail.com>
2020-06-06 00:11:58 +0800

    Add wait to wait child, and some comments

--- hw5/part2/main.c
+++ hw5/part2/main.c
@@ -1,13 +1,27 @@
 #include "read.h"
 #include "write.h"
 
+/**
+ * Reference:
+ * https://courses.cs.washington.edu/courses/cse451/06au/slides/mmap-copy.c
+ * https://stackoverflow.com/questions/26259421/use-mmap-in-c-to-write-into-memory
+ * https://man7.org/linux/man-pages/man2/mmap.2.html
+ */
+
 int main()
 {
+    /**
+     * Generate 2 process to communication with mmap,
+     * each process map the "foo" file into their memory space.
+     *
+     * Yeah, it's immediately.
+     * Why? Because parent wait for child read done and exit.
+     */
     pid_t pid = fork();
-    if(pid){
-        testWrite("foo", "junk");
-    }
-    else{
+    if (pid) {
+        testWrite("foo", "junk\n");
+        waitpid(pid, NULL, 0);
+    } else {
         char buf[sysconf(_SC_PAGESIZE)];
         testRead("foo", buf);
         printf("READ: %s", buf);
