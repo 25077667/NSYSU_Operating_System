55a0d1cefd7e57b8a275d21757999b7c9385ee8b
25077667 <zxc25077667@gmail.com>
2020-06-06 00:08:09 +0800

    Add comments and sync

--- hw5/part2/read.c
+++ hw5/part2/read.c
@@ -1,11 +1,12 @@
 #include "read.h"
 
-void testRead(const char *filename, void* buf)
+void testRead(const char *filename, void *buf)
 {
     int fd;
     struct stat sb;
 
     fd = open(filename, O_RDONLY);
+    /* Bitwise operation to identify is -1 */
     if (!(~fd))
         handle_error("Open");
 
@@ -17,10 +18,9 @@ void testRead(const char *filename, void* buf)
     if (addr == MAP_FAILED)
         handle_error("mmap");
 
-    fstat(fd, &sb);
-    if(!sb.st_size)
-        handle_error("Empty");
-    memcpy(buf,addr, getpagesize());
+    /* Make sure it's up to date before copy */
+    msync(addr, getpagesize(), MS_SYNC);
+    memcpy(buf, addr, getpagesize());
 
     munmap(addr, getpagesize());
     close(fd);
