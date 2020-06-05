a0ad2fba3d4ed2aa27b0ef4150233f5e0ac3af18
25077667 <zxc25077667@gmail.com>
2020-06-05 21:10:55 +0800

    Add read.[ch]

---
+++ hw5/part2/read.h
@@ -0,0 +1,26 @@
+#ifndef READ_H
+#define READ_H
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+#define handle_error(msg)   \
+    do {                    \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
+
+/*
+ * To test mmapping a file.
+ * 
+ * @filename: the file you want to mmap and read as char*
+ * @buf: The buffer you provide to contain readed in data.
+ */
+void testRead(const char *filename, void *buf);
+
+#endif
