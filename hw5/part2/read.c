922a34429e570ae84e5645d59cc01aef4f0f726a
25077667 <zxc25077667@gmail.com>
2020-06-17 12:02:00 +0800

    Close fd first in read.c

--- hw5/part2/read.c
+++ hw5/part2/read.c
@@ -10,13 +10,12 @@ void testRead(const char *filename, void *buf)
         handle_error("read open");
 
     void *addr = mmap(NULL, getpagesize(), PROT_READ, MAP_SHARED, fd, 0);
+    /* Close fd first, Be sure it read from memory immediately. */
+    close(fd);
+
     if (addr == MAP_FAILED)
         handle_error("mmap");
 
-
     memcpy(buf, addr, getpagesize());
-    msync(addr, getpagesize(), MS_SYNC);
-
     munmap(addr, getpagesize());
-    close(fd);
 }
